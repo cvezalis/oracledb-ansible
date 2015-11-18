ce51336de4ff8584ebecda80c4125c88d753c752
cvezalis <christos.vezalis@gmail.com>
2015-11-18 13:53:26 +0200

    Add a check before set the kernel variables

--- roles/linux-oracle/files/disable_trans_hugepages.sh
+++ roles/linux-oracle/files/disable_trans_hugepages.sh
@@ -9,5 +9,9 @@ if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
 fi
 EOF
 
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
+if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+    echo never > /sys/kernel/mm/transparent_hugepage/enabled
+fi
+if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
+    echo never > /sys/kernel/mm/transparent_hugepage/defrag
+fi
